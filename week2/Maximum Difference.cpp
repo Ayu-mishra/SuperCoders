196543d9208156e31f8f41c17b941fb0e592772f
Anurag Mishra <anuragayumishra@gmail.com>
2022-05-17 00:20:25 +0530

    Update Maximum Difference.cpp

--- week2/Maximum Difference.cpp
+++ week2/Maximum Difference.cpp
@@ -1,31 +1,13 @@
-#include<iostream>
-#include<cstdio>
-#include<cmath>
-using namespace std;
-
 int maxDifference(int arr[], int n){
- 
-  int maxDiff=arr[1]-arr[0];
-  int minElem=arr[0];
-  for(int i=1;i<n;i++){
-    if(arr[i]-minElem>maxDiff){
-      maxDiff=arr[i]-minElem; 
-    }
-   if(arr[i]<minElem)
-     minElem=arr[i];
+  // complete this function
+  int max = 0, min=arr[0];
+  for(int i=1; i<n; i++)
+  {
+    if(arr[i]-min >max) max = arr[i] - min;
+    if(arr[i]<min) min = arr[i];
+    
   }
-  if(maxDiff <=0) maxDiff = -1;
-  return maxDiff;
-}
-
-int main() {
-  int n;
-  cin>>n;
-  int arr[n];
-  int i;
-  for(i=0;i<n;i++)
-    cin>>arr[i];
-  int res = maxDifference(arr,n);
-  cout<<res;
-  return 0;
+  if(max<=0) max = -1;
+  return max;
+  
 }
